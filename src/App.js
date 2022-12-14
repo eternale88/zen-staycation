0d80de7579dc7e439613ca17e57dd63242e4ee8e
eternale88 <ethanrobbins@ymail.com>
2022-12-13 22:10:59 -0500

    upgrade react router to 6

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
-import { Home, Products, SingleProduct, Cart, Error, About, PrivateRoute, Checkout, AuthWrapper} from './pages'
+import {
+  Home,
+  Products,
+  SingleProduct,
+  Cart,
+  Error,
+  About,
+  PrivateRoute,
+  Checkout,
+  AuthWrapper,
+} from './pages'
 
 function App() {
   return (
     //authWrapper gives app enough time to handle auth, and when data is correct(checked in the private checkout route) - then pass children
     <AuthWrapper>
       <Router>
-        <Navbar/>
-        <Sidebar/>
-        <Switch>
-          <Route exact path='/'>
-            <Home/>
-          </Route>
-          <Route exact path='/about'>
-            <About/>
-          </Route>
-          <Route exact path='/cart'>
-            <Cart/>
-          </Route>
-          <Route exact path='/products'>
-            <Products/>
-          </Route>
-          <Route exact path='/products/:id' children={<SingleProduct/>}/>
-          <PrivateRoute exact path='/checkout'>
-            <Checkout/>
-          </PrivateRoute>
-          <Route exact path='*'>
-            <Error/>
-          </Route>
-        </Switch>
-        <Footer/>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<SingleProduct />} />
+          <Route
+            path="/checkout"
+            element={
+              <PrivateRoute>
+                <Checkout />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
       </Router>
     </AuthWrapper>
-    
   )
 }
 
